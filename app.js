9b348542cf7ec4acaaef7c574e88e56a2d745a88
Stone595 <stone.ruiz2927@stu.mtec.edu>
2021-10-11 21:55:39 -0600

    add functionallity to create task

--- app.js
+++ app.js
@@ -18,20 +18,20 @@ let createId = (function(){
 const listContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form]');
 const newListName = document.querySelector('[data-new-list-name]');
+const taskBody = document.querySelector('[data-tasks]');
+const newTask = document.querySelector('[data-create-task]');
 const deleteListButton = document.querySelector('[data-delete-list-btn]');
 const displayContainer = document.querySelector('[data-display-container]');
 const listTitle = document.querySelector('[data-list-title]');
 const listCount = document.querySelector('[data-list-count]');
 const listTasks = document.querySelector('[data-tasks');
+const taskElement = document.getElementById('taskTemplate');
 
 const LOCALSTORAGELISTKEY = "LISTSKEY";
 const LOCALSTORAGESELECTELIST = "SELECTIEDLIST";
-
 let templists = localStorage.getItem(LOCALSTORAGELISTKEY)
 let lists = JSON.parse(templists) || [{id: 'tests', name :'testing'}];
-
 let selectedList = JSON.parse(localStorage.getItem('LOCALSTORAGESELECTELIST'));
-
 
 listContainer.addEventListener('click', x => {
   if(x.target.tagName.toLowerCase() === 'li'){
@@ -40,8 +40,6 @@ listContainer.addEventListener('click', x => {
     render();
   }
 })
-
-
 
 newListForm.addEventListener('submit', x => {
   x.preventDefault(); 
@@ -58,7 +56,7 @@ newListForm.addEventListener('submit', x => {
 })
 
 function createNewList(name) {
-  return {id: createId(), name: name, tasks: [{id: 1, name: 'yourMom', completed: false}]}
+  return {id: createId(), name: name, tasks: []}
 }
 
 function save() {
@@ -69,17 +67,13 @@ function save() {
 function render(){
   renderLists();
   let selectedListObj = lists.find(list => list.id === selectedList);
-
   if(selectedList == null){
     displayContainer.style.display = 'none'; 
   } else {
     displayContainer.style.display = '';
-    
     listTitle.innerHTML = '<h2 data-list-title >'+ selectedListObj.name + '</h2>';
-    renderTasksCount(selectedListObj)
+    renderTasks(selectedListObj);
   }
-  
-  
 }
 
 function renderLists() {
@@ -95,17 +89,56 @@ function renderLists() {
       listHTML.classList.add('activeList')
     }
   })
+  clearElemnts(taskBody);
+  
 }
+function addNewTasks() {
+  let selectedListObj = lists.find(list => list.id === selectedList);
+  const taskName = newTask.value;
+  console.log(newTask.value)
+  if(!taskName || taskName == ""){
+    console.log('failed')
+    return
+  } else {
+    let task = createTask(taskName);
+    // const selectedList1 = lists.find(list => list.id === selectedListObj.name);
+    
+    selectedListObj.tasks.push(task)
+    newTask.value =''; 
+    render();
+    // renderTasks(selectedListObj);
+    save();
+  }
+}
+function renderTasks(selectedList) {
+  selectedList.tasks.forEach(task =>{
+    let taskHtml = document.importNode(taskElement.content, true)
+    let actCheckBox = taskHtml.querySelector('input');
+    actCheckBox.id = task.id; 
+    actCheckBox.checked = task.completed;
+    let taskLabel = taskHtml.querySelector('label');
+    taskLabel.htmlfor = task.id; 
+    taskLabel.append(task.name);
+    taskBody.appendChild(taskHtml)
+  })
+
+}
+
+function createTask(name){
+  return {id: createId(), name: name, completed: false}
+}
+
 function clearElemnts(element) {
   while(element.firstChild){
     element.removeChild(element.firstChild);
   }
 }
+
 deleteListButton.addEventListener('click', () => {
   console.log(selectedList)
   lists = lists.filter(list => list.id !== selectedList);
-  console.log(lists)
   
+  console.log(lists)
   render();
   save();
 })
